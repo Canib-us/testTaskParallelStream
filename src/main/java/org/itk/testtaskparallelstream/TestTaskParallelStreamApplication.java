de4bec47991314136583c6cb28eb3c1c5ad109be
Canib-us <balev.danilll@gmail.com>
2025-08-05 22:33:44 +0700

    Initial commit

---
+++ src/main/java/org/itk/testtaskparallelstream/TestTaskParallelStreamApplication.java
@@ -0,0 +1,13 @@
+package org.itk.testtaskparallelstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskParallelStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskParallelStreamApplication.class, args);
+    }
+
+}
